360ca08a2596183f726e82b7f6423edcbaf48518
JuanDavinLie <juandavinlie88@gmail.com>
2022-03-07 17:12:00 +0800

    add example

---
+++ Example/Shared/LibraryUsingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryUsingAppApp.swift
+//  Shared
+//
+//  Created by Juan Lie on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LibraryUsingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
